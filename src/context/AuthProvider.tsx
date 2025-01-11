38c400a7c69d8492a5fe540cdffc753e7ad2010f
Rokdeshwar Jadhav <106864522+lokesh7887@users.noreply.github.com>
2025-01-11 23:38:09 +0530

    Update AuthProvider.tsx

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -4,7 +4,8 @@ import { SessionProvider } from 'next-auth/react';
 
 export default function AuthProvider({
   children,
-}: {
+}: 
+                                     {
   children: React.ReactNode;
 }) {
   return (
